bab83b986a5de6a9368412d6bc6c9f0e066d8936
EM-H20 <ghd0106@gmail.com>
2025-07-28 15:48:26 +0900

    init

---
+++ section02/src/chapter2.ts
@@ -0,0 +1,20 @@
+// 배열
+let numArr : number[] = [1,2,3];
+
+let strArr : string[] = ["hello","world"];
+
+// 제네릭 분법 Array<T>
+let boolArr : Array<boolean> = [true,false];
+
+// 배열에 들어가는 요소들의 타입이 다양할 경우
+let multiArr : (number | string)[] = [1, "hello"];
+
+// 다차원 배열의 타입을 정의하는 방법
+let doubleArr : number[][] = [
+    [1,2,3],
+    [4,5,6],
+];
+
+// 튜플
+let tup1 : [number, number] = [1,2];
+
